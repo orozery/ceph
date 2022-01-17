12f93f35eb5e030bc4192c16bfcb4eaf1fda45c1
Or Ozeri <oro@il.ibm.com>
2022-01-17 06:53:55 +0200

    librbd/crypto: allow loading luks format without specifying version
    This commits extends rbd API's to allow the user to load encryption
    using either LUKS1 or LUKS2 formats, without explicitly specifying which.
    Prior, the user had to specify explicitly LUKS1 or LUKS2.
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- src/test/librbd/test_librbd.cc
+++ src/test/librbd/test_librbd.cc
@@ -2248,6 +2248,10 @@ TEST_F(TestLibRBD, TestEncryptionLUKS1)
           .passphrase = "password",
           .passphrase_size = 8,
   };
+  rbd_encryption_luks_format_options_t luks_opts = {
+          .passphrase = "password",
+          .passphrase_size = 8,
+  };
   ASSERT_EQ(0, rbd_open(ioctx, name.c_str(), &image, NULL));
 
 #ifndef HAVE_LIBCRYPTSETUP
@@ -2270,7 +2274,7 @@ TEST_F(TestLibRBD, TestEncryptionLUKS1)
 
   ASSERT_EQ(0, rbd_open(ioctx, name.c_str(), &image, NULL));
   ASSERT_EQ(0, rbd_encryption_load(
-          image, RBD_ENCRYPTION_FORMAT_LUKS1, &opts, sizeof(opts)));
+          image, RBD_ENCRYPTION_FORMAT_LUKS, &luks_opts, sizeof(luks_opts)));
   read_test_data(image, "test", 0, 4, 0, &passed);
   ASSERT_TRUE(passed);
 #endif
@@ -2300,6 +2304,10 @@ TEST_F(TestLibRBD, TestEncryptionLUKS2)
           .passphrase = "password",
           .passphrase_size = 8,
   };
+  rbd_encryption_luks_format_options_t luks_opts = {
+          .passphrase = "password",
+          .passphrase_size = 8,
+  };
   ASSERT_EQ(0, rbd_open(ioctx, name.c_str(), &image, NULL));
 
 #ifndef HAVE_LIBCRYPTSETUP
@@ -2307,7 +2315,13 @@ TEST_F(TestLibRBD, TestEncryptionLUKS2)
           image, RBD_ENCRYPTION_FORMAT_LUKS2, &opts, sizeof(opts)));
   ASSERT_EQ(-ENOTSUP, rbd_encryption_load(
           image, RBD_ENCRYPTION_FORMAT_LUKS2, &opts, sizeof(opts)));
+  ASSERT_EQ(-ENOTSUP, rbd_encryption_format(
+          image, RBD_ENCRYPTION_FORMAT_LUKS, &luks_opts, sizeof(luks_opts)));
+  ASSERT_EQ(-ENOTSUP, rbd_encryption_load(
+          image, RBD_ENCRYPTION_FORMAT_LUKS, &luks_opts, sizeof(luks_opts)));
 #else
+  ASSERT_EQ(-EINVAL, rbd_encryption_format(
+          image, RBD_ENCRYPTION_FORMAT_LUKS, &luks_opts, sizeof(luks_opts)));
   ASSERT_EQ(0, rbd_encryption_format(
           image, RBD_ENCRYPTION_FORMAT_LUKS2, &opts, sizeof(opts)));
   ASSERT_EQ(-EEXIST, rbd_encryption_load(
@@ -2322,7 +2336,7 @@ TEST_F(TestLibRBD, TestEncryptionLUKS2)
 
   ASSERT_EQ(0, rbd_open(ioctx, name.c_str(), &image, NULL));
   ASSERT_EQ(0, rbd_encryption_load(
-          image, RBD_ENCRYPTION_FORMAT_LUKS2, &opts, sizeof(opts)));
+          image, RBD_ENCRYPTION_FORMAT_LUKS, &luks_opts, sizeof(luks_opts)));
   read_test_data(image, "test", 0, 4, 0, &passed);
   ASSERT_TRUE(passed);
 #endif
