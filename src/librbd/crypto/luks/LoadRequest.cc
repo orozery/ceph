f5cc17dc3c1c32f67fbbea680140d0624d08fb53
Or Ozeri <oro@il.ibm.com>
2022-01-17 08:01:11 +0200

    librbd/crypto: add further debug loggings
    Add further useful debug loggings to librbd crypto code.
    Additionally, increase libcryptsetup debug level to 30 (as it prints out to stdout).
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- src/librbd/crypto/luks/LoadRequest.cc
+++ src/librbd/crypto/luks/LoadRequest.cc
@@ -71,6 +71,8 @@ void LoadRequest<I>::read(uint64_t end_offset, Context* on_finish) {
 
 template <typename I>
 bool LoadRequest<I>::handle_read(int r) {
+  ldout(m_image_ctx->cct, 20) << "r=" << r << dendl;
+
   if (r < 0) {
     lderr(m_image_ctx->cct) << "error reading from image: " << cpp_strerror(r)
                             << dendl;
@@ -92,6 +94,8 @@ bool LoadRequest<I>::handle_read(int r) {
 
 template <typename I>
 void LoadRequest<I>::handle_read_header(int r) {
+  ldout(m_image_ctx->cct, 20) << "r=" << r << dendl;
+
   if (!handle_read(r)) {
     return;
   }
@@ -150,6 +154,8 @@ void LoadRequest<I>::handle_read_header(int r) {
 
 template <typename I>
 void LoadRequest<I>::handle_read_keyslots(int r) {
+  ldout(m_image_ctx->cct, 20) << "r=" << r << dendl;
+
   if (!handle_read(r)) {
     return;
   }
@@ -188,6 +194,8 @@ void LoadRequest<I>::read_volume_key() {
 
 template <typename I>
 void LoadRequest<I>::finish(int r) {
+  ldout(m_image_ctx->cct, 20) << "r=" << r << dendl;
+  
   *m_format_mismatch = m_header.is_bad_magic();
   ceph_memzero_s(&m_passphrase[0], m_passphrase.size(), m_passphrase.size());
   m_on_finish->complete(r);
