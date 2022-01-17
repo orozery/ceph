e3d47856d25254e058a3d5f9ea41954f66d0ca28
Or Ozeri <oro@il.ibm.com>
2022-01-17 08:01:11 +0200

    librbd/crypto: add further debug loggings
    Add further useful debug loggings to librbd crypto code.
    Additionally, increase libcryptsetup debug level to 30 (as it prints out to stdout).
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- src/librbd/crypto/LoadRequest.cc
+++ src/librbd/crypto/LoadRequest.cc
@@ -67,6 +67,9 @@ void LoadRequest<I>::load() {
 
 template <typename I>
 void LoadRequest<I>::handle_load(int r) {
+  ldout(m_image_ctx->cct, 20) << "r=" << r << ". image name: "
+                              << m_current_image_ctx->name << dendl;
+
   if (r < 0) {
     if (m_is_current_format_cloned && m_format_mismatch) {
       // encryption format was not detected, assume plaintext
@@ -99,6 +102,8 @@ void LoadRequest<I>::handle_load(int r) {
 
 template <typename I>
 void LoadRequest<I>::finish(int r) {
+  ldout(m_image_ctx->cct, 20) << "r=" << r << dendl;
+
   if (r == 0) {
     auto ictx = m_image_ctx;
     for (auto& format : m_formats) {
